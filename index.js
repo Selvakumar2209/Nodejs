2802ac7d3cf55e35e64f252786318af4e7a2598a
Selvakumar A <59720344+Selvakumar2209@users.noreply.github.com>
2025-06-06 20:14:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,14 +9,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('🚀 Hello from DevOps CI/CD Pipeline!');
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
